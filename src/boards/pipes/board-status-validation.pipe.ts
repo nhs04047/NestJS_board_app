4253471405a9245c383ce81c9747794e086c838e
보현 김 <nhs04047@gmail.com>
2022-07-18 01:12:29 +0900

    feat : status 검사 커스텀 pipe 생성

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,7 +1,21 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  readonly StatusOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOption.indexOf(status);
+    return index !== -1;
   }
 }
