73e5698e5b2033a4f5b3241ae967eb848ddc87cb
보현 김 <nhs04047@gmail.com>
2022-07-19 02:51:17 +0900

    feat : 로그인 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,9 @@ export class AuthController {
   ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) AuthCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signIn(AuthCredentialsDto);
+  }
 }
