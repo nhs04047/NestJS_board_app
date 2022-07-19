78c53a2b4571bdad27df9e13ca7070e9c3dc3a4a
보현 김 <nhs04047@gmail.com>
2022-07-20 02:36:35 +0900

    feat : movie 컨트롤러, 서비스 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMconfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMconfig), BoardsModule, AuthModule],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
 export class AppModule {}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Movie } from './movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('id')
+  remove(@Param('id') id: string) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') id: string, @Body() updateData) {
+    return `This will patch a movie with the id: ${id}`;
+  }
+}
